9333d38821b9e49da7a8be24b2ee744a995d48c4
fspinar <fabian.spinar@hallowelt.com>
2024-09-30 11:59:45 +0200

    (fspinar) fix runjobs for farm

--- root-fs/app/bin/config/runJobs_settings.php
+++ root-fs/app/bin/config/runJobs_settings.php
@@ -1,7 +1,7 @@
 <?php
 $GLOBALS['wgDBtype'] = "mysql";
-$GLOBALS['wgDBserver'] =###DB_HOST###;
-$GLOBALS['wgDBname'] =###DB_NAME###;
-$GLOBALS['wgDBuser'] =###DB_USER###;
-$GLOBALS['wgDBpassword'] =###DB_PASS###;
-$GLOBALS['wgDBprefix'] =###DB_PREFIX###;
+$GLOBALS['wgDBserver'] ="###DB_HOST###";
+$GLOBALS['wgDBname'] ="###DB_NAME###";
+$GLOBALS['wgDBuser'] ="###DB_USER###";
+$GLOBALS['wgDBpassword'] ="###DB_PASS###";
+$GLOBALS['wgDBprefix'] ="###DB_PREFIX###";
